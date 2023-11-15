757f045e5bc06cf97f121ae46de995a6c1144683
emi-river <emi.riverdi@gmail.com>
2023-11-15 12:05:46 +0100

    WIP

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,7 @@
 import "./App.css";
 
 function App() {
-  return (
-    <>
-      <div>
-        <p className="motto">Spill the social tea</p>
-      </div>
-    </>
-  );
+  return <></>;
 }
 
 export default App;
---
+++ frontend/src/components/Register.jsx
@@ -0,0 +1,13 @@
+import "../index.css";
+import "../App.css";
+function Register() {
+  return (
+    <>
+      <div>
+        <div className="background-icon"></div>
+      </div>
+    </>
+  );
+}
+
+export default Register;
