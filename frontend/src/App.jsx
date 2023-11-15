c3bf5b6316627267d2562c93df41cb4306572516
awaltherr <anton.walther@live.com>
2023-11-15 14:49:16 +0100

    First version of signin page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,26 @@
 import "./App.css";
+import "./index.css";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <div className="container">
+        <div className="background-icon"></div>
+        <div className="signin-container">
+          <h1 className="signin-title">This is Social Tea</h1>
+          <input type="text" placeholder="Username" />
+          <input type="text" placeholder="Placeholder" />
+          <div className="forgot-password-text">
+            <p>Forgot password?</p>
+          </div>
+          <div className="signin-action-btns">
+            <button>Sign In</button>
+            <button>Register</button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default App;
