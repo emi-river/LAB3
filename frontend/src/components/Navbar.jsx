238bbf261fd1d33d9f597965e3f45805ffc8eb34
awaltherr <anton.walther@live.com>
2023-11-21 20:58:40 +0100

    Added sticky navbar in PostWall

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,27 @@
+import '../index.css'
+import './Navbar.css'
+import { Link } from 'react-router-dom'
+
+function Navbar() {
+  return (
+    <>
+      <div className="navbar-container">
+        <div className="left-side">
+          <Link to="#">
+            <h1>Social Tea</h1>
+          </Link>
+        </div>
+        <div className="right-side">
+          <Link to="#">
+            <h3>Settings</h3>
+          </Link>
+          <Link to="/">
+            <h3>Sign out</h3>
+          </Link>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Navbar
--- frontend/src/components/PostWall.jsx
+++ frontend/src/components/PostWall.jsx
@@ -1,50 +1,54 @@
-import "../index.css";
-import Post from "./PostTimeline";
-import PostCreation from "./PostCreation";
+import '../index.css'
+import Post from './PostTimeline'
+import PostCreation from './PostCreation'
+import Navbar from './Navbar'
 
-import profileImage1 from "../assets/profileImage1.jpg";
-import profileImage2 from "../assets/profileImage2.jpg";
-import profileImage3 from "../assets/profileImage3.jpg";
-import postImage1 from "../assets/postImage1.jpg";
-import postImage2 from "../assets/postImage2.jpg";
-import postImage3 from "../assets/postImage3.jpg";
+import profileImage1 from '../assets/profileImage1.jpg'
+import profileImage2 from '../assets/profileImage2.jpg'
+import profileImage3 from '../assets/profileImage3.jpg'
+import postImage1 from '../assets/postImage1.jpg'
+import postImage2 from '../assets/postImage2.jpg'
+import postImage3 from '../assets/postImage3.jpg'
 
 const PostsWall = () => {
   const posts = [
     {
       postId: 1,
-      username: "hejsan",
-      description: "Paraply",
+      username: 'hejsan',
+      description: 'Paraply',
       profileImage: profileImage1,
       postedImage: postImage1,
-      uploadTime: "2 min ago",
+      uploadTime: '2 min ago'
     },
     {
       postId: 2,
-      username: "svejsan",
-      description: "Lampa",
+      username: 'svejsan',
+      description: 'Lampa',
       profileImage: profileImage2,
       postedImage: postImage2,
-      uploadTime: "6 min ago",
+      uploadTime: '6 min ago'
     },
     {
       postId: 3,
-      username: "example",
-      description: "Zen zone",
+      username: 'example',
+      description: 'Zen zone',
       profileImage: profileImage3,
       postedImage: postImage3,
-      uploadTime: "10 min ago",
-    },
-  ];
+      uploadTime: '10 min ago'
+    }
+  ]
 
   return (
-    <div className="posts">
-      <PostCreation posts={posts} />
-      {posts.map((post) => (
-        <Post post={post} key={post.id} />
-      ))}
-    </div>
-  );
-};
+    <>
+      <Navbar />
+      <div className="posts">
+        <PostCreation posts={posts} />
+        {posts.map((post) => (
+          <Post post={post} key={post.id} />
+        ))}
+      </div>
+    </>
+  )
+}
 
-export default PostsWall;
+export default PostsWall
