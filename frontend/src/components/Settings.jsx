663d3139ab4c1c63113720c526b8f8292bfda734
CodeByJohan <johan.gotet@gmail.com>
2023-11-24 00:49:39 +0100

    Add label

--- frontend/src/components/Settings.jsx
+++ frontend/src/components/Settings.jsx
@@ -57,28 +57,40 @@ function Settings() {
         personDetails.map((personDetail, index) => {
           return (
             <div className="settings" key={index}>
-              <input
-                defaultValue={personDetail.username}
-                onChange={(event) => setUserName(event.target.value)}
-                placeholder="Username"
-              />
-              <input
-                defaultValue={personDetail.email}
-                onChange={(event) => setEmail(event.target.value)}
-                type="email"
-                placeholder="Email"
-              />
-              <input
-                defaultValue={personDetail.password}
-                onChange={(event) => setPassword(event.target.value)}
-                type="password"
-                placeholder="Password"
-              />
-              <input
-                defaultValue={personDetail.bio}
-                onChange={(event) => setBio(event.target.value)}
-                placeholder="Bio text"
-              />
+              <label>
+                Username
+                <input
+                  defaultValue={personDetail.username}
+                  onChange={(event) => setUserName(event.target.value)}
+                  placeholder="Username"
+                />
+              </label>
+              <label>
+                Email
+                <input
+                  defaultValue={personDetail.email}
+                  onChange={(event) => setEmail(event.target.value)}
+                  type="email"
+                  placeholder="Email"
+                />
+              </label>
+              <label>
+                Password
+                <input
+                  defaultValue={personDetail.password}
+                  onChange={(event) => setPassword(event.target.value)}
+                  type="password"
+                  placeholder="Password"
+                />
+              </label>
+              <label>
+                Bio
+                <input
+                  defaultValue={personDetail.bio}
+                  onChange={(event) => setBio(event.target.value)}
+                  placeholder="Bio text"
+                />
+              </label>
               <div className="setting-buttons">
                 <button onClick={updateDetails}>Update</button>
                 <button onClick={deleteDetails}>Delete</button>
