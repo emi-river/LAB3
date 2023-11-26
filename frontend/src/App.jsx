11276e449daf4bd0314882984d8722e670fdd842
CodeByJohan <johan.gotet@gmail.com>
2023-11-26 17:14:00 +0100

    Add lazy-with-preload to preload the components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,18 @@
 import './App.css'
-import Home from './components/Home'
-import Register from './components/Register'
-import ForgotPassword from './components/ForgotPassword'
-import Settings from './components/Settings'
-import PostWall from './components/PostWall'
+import { Suspense } from 'react'
+import { lazyWithPreload } from 'react-lazy-with-preload'
+
+const Home = lazyWithPreload(() => import('./components/Home'))
+const Register = lazyWithPreload(() => import('./components/Register'))
+const ForgotPassword = lazyWithPreload(() =>
+  import('./components/ForgotPassword')
+)
+const Settings = lazyWithPreload(() => import('./components/Settings'))
+const PostWall = lazyWithPreload(() => import('./components/PostWall'))
+const Profile = lazyWithPreload(() => import('./components/Profile'))
+const Terms = lazyWithPreload(() => import('./GDPR/Terms'))
+const Condition = lazyWithPreload(() => import('./GDPR/Condition'))
+const Policy = lazyWithPreload(() => import('./GDPR/Policy'))
 
 import {
   createHashRouter,
@@ -12,11 +21,6 @@ import {
   Navigate
 } from 'react-router-dom'
 import PropTypes from 'prop-types'
-
-import Profile from './components/Profile'
-import Terms from './GDPR/Terms'
-import Condition from './GDPR/Condition'
-import Policy from './GDPR/Policy'
 
 function Root() {
   return <Outlet />
@@ -41,29 +45,91 @@ function App() {
   const router = createHashRouter([
     {
       children: [
-        { element: <Home />, path: '/' },
-        { element: <Register />, path: '/register' },
-        { element: <ForgotPassword />, path: '/forgotpassword' },
-        { element: <Settings />, path: '/settings/:id' },
+        {
+          element: (
+            <Suspense fallback={<>Loading....</>}>
+              <Home />
+            </Suspense>
+          ),
+          path: '/'
+        },
+        {
+          element: (
+            <Suspense fallback={<>Loading....</>}>
+              <Register />
+            </Suspense>
+          ),
+          path: '/register'
+        },
+        {
+          element: (
+            <Suspense fallback={<>Loading....</>}>
+              <ForgotPassword />
+            </Suspense>
+          ),
+          path: '/forgotpassword'
+        },
+        {
+          element: (
+            <Suspense fallback={<>Loading....</>}>
+              <Settings />
+            </Suspense>
+          ),
+          path: '/settings/:id'
+        },
 
         {
           children: [
             {
               element: (
-                <PrivateRoute>
+                <Suspense fallback={<>Loading....</>}>
                   <PostWall />
-                </PrivateRoute>
+                </Suspense>
               ),
               path: '/postwall'
             }
           ]
         },
-
-        { element: <PostWall />, path: '/postwall/:id' },
-        { element: <Terms />, path: '/terms' },
-        { element: <Condition />, path: '/condition' },
-        { element: <Policy />, path: '/policy' },
-        { element: <Profile />, path: '/profile/:id' }
+        {
+          element: (
+            <Suspense fallback={<>Loading....</>}>
+              <PostWall />
+            </Suspense>
+          ),
+          path: '/postwall/:id'
+        },
+        {
+          element: (
+            <Suspense fallback={<>Loading....</>}>
+              <Profile />
+            </Suspense>
+          ),
+          path: '/profile/:id'
+        },
+        {
+          element: (
+            <Suspense fallback={<>Loading....</>}>
+              <Terms />
+            </Suspense>
+          ),
+          path: '/terms'
+        },
+        {
+          element: (
+            <Suspense fallback={<>Loading....</>}>
+              <Condition />
+            </Suspense>
+          ),
+          path: '/condition'
+        },
+        {
+          element: (
+            <Suspense fallback={<>Loading....</>}>
+              <Policy />
+            </Suspense>
+          ),
+          path: '/policy'
+        }
       ],
       element: <Root />
     }
